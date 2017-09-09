75c79e17991782b0016ae5118d98cddd9f5785ad
Tim3Game <timgamer2012gmail.com>
2017-09-09 13:12:13 +0200

    Added Spigot Repo & Dependency + added Main.java

---
+++ src/main/java/eu/tbeihofner/minecraft_test/Main.java
@@ -0,0 +1,14 @@
+package eu.tbeihofner.minecraft_test;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
